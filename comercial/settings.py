8fe057be59898bb1a788e24a440274f9f44dcf49
leyder <leyderjmh@gmail.com>
2025-06-09 17:30:31 -0500

    cambios2 lunes

--- comercial/settings.py
+++ comercial/settings.py
@@ -6,9 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-reemplazar-esto')
 
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+ALLOWED_HOSTS = ["*", "comercializadora-f2cm.onrender.com"]
+
 
 # Agrega el hostname de Render si existe
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
